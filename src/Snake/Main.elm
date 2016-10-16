1971b0d3547b5bc8839410b3c2fedf969e3b02c3
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2016-10-16 14:59:39 +0200

    Model definition

--- src/Snake/Main.elm
+++ src/Snake/Main.elm
@@ -2,6 +2,9 @@ module Snake.Main exposing (main)
 
 import Html exposing (..)
 import Html.App
+import Keyboard exposing (..)
+
+import List.Nonempty as NeL exposing (fromElement, Nonempty, (:::))
 
 import Snake.Model exposing (..)
 
@@ -15,12 +18,24 @@ main =
         }
 
 type alias Model =
-  {}
+  { status : Status
+  , food : Point
+  , snake : Snake
+  , direction : Direction
+  }
 
-type Msg = Tick | SetDirection Direction
+type Msg = Tick | SetDirection Direction | NoOp
+
+maxPoint : Point
+maxPoint = (16, 16)
 
 initialModel : Model
-initialModel = {}
+initialModel =
+  { status = Paused
+  , food = (0, 0)
+  , snake = Snake ((7, 8) ::: ((8, 8) ::: (fromElement (9, 8)))) False
+  , direction = Right
+  }
 
 init : (Model, Cmd Msg)
 init = ( initialModel, Cmd.none )
@@ -28,13 +43,21 @@ init = ( initialModel, Cmd.none )
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        _ ->
-            ( model, Cmd.none )
+      SetDirection direction -> ( { model | direction = direction } , Cmd.none )
+      Tick -> ( tick model, Cmd.none )
 
 view : Model -> Html Msg
-view model = text (toString model)
-
+view model =
+  div
+    []
+    [ text (toString model) ]
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Keyboard.presses (\code -> case code of
+      97 -> SetDirection Left
+      44 -> SetDirection Up
+      101 -> SetDirection Right
+      111 -> SetDirection Down
+      32 -> Tick
+      _ -> NoOp)
--- src/Snake/Model.elm
+++ src/Snake/Model.elm
@@ -1,7 +1,11 @@
-module Snake.Model exposing (Direction)
+module Snake.Model exposing (..)
 
-
+import List.Nonempty as NeL exposing (Nonempty, (:::))
 
 type Direction = Right | Up | Left | Down
 
 type alias Point = (Int, Int)
+
+type Snake = Snake (Nonempty Point) Bool
+
+type Status = Active Int | Paused | Finished
